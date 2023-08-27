8b63594683d0942a77632bbd383b6be2465f9ec9
Kim <kmdnpm@gmail.com>
2023-08-27 00:11:25 -0400

    functioning page cycling

--- script.js
+++ script.js
@@ -1,41 +1,67 @@
 const loginForm = document.getElementById('login-form');
 const dashboard = document.getElementById('dashboard');
 const logoutButton = document.getElementById('logout-button');
-const resetPassword = document.getElementById('passwordRecovery-link')
-const signUP = document.getElementById('createAccount-link')
+const resetPassword = document.getElementById('pwdRec-link')
+const newSignUp = document.getElementById('newAcc-link')
+const verify = document.getElementById('verification-link')
 
-resetPassword.addEventListener('click', () => {
-    window.location.href = 'passwordRecovery.html';
-});
+// resetPassword.addEventListener('click', () => {
+//     window.location.href = 'passwordRecovery.html';
+// });
 
-signUP.addEventListener('click', () => {
-    window.location.href = 'signUP.html';
-});
+// newSignUp.addEventListener('click', () => {
+//     window.location.href = 'signUP.html';
+// });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".navigation-button");
 
-loginForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // In a real application, you'd validate user credentials here.
-    // For this example, we'll assume a simple username and password.
-    if (username === 'user' && password === 'password') {
-        loginForm.style.display = 'none';
-        dashboard.style.display = 'block';
-    } else {
-        alert('Invalid credentials. Please try again.');
-    }
-});
-
-logoutButton.addEventListener('click', function () {
-    loginForm.style.display = 'block';
-    dashboard.style.display = 'none';
-});
-
-const newsBanners = document.querySelectorAll('.news-banner');
-newsBanners.forEach(banner => {
-    banner.addEventListener('click', function () {
-        alert(`Clicked on ${banner.innerText}`);
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            const nextPage = this.getAttribute("data-next-page");
+            window.location.href = nextPage;
+        });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".navigation-button2");
+
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            const nextPage = this.getAttribute("data-next-page");
+            window.location.href = nextPage;
+        });
+    });
+});
+
+verify.addEventListener('click', () => {
+    window.location.href = 'verificationLink.html';
+});
+
+// loginForm.addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+//     // In a real application, you'd validate user credentials here.
+//     // For this example, we'll assume a simple username and password.
+//     if (username === 'user' && password === 'password') {
+//         loginForm.style.display = 'none';
+//         dashboard.style.display = 'block';
+//     } else {
+//         alert('Invalid credentials. Please try again.');
+//     }
+// });
+
+// logoutButton.addEventListener('click', function () {
+//     loginForm.style.display = 'block';
+//     dashboard.style.display = 'none';
+// });
+
+// const newsBanners = document.querySelectorAll('.news-banner');
+// newsBanners.forEach(banner => {
+//     banner.addEventListener('click', function () {
+//         alert(`Clicked on ${banner.innerText}`);
+//     });
+// });
